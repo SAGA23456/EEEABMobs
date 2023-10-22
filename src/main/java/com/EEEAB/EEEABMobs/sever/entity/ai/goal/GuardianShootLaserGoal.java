5230d8b27e3c4c98b6a83fd67cd1c706911b235b
EEEAB <2055526030@qq.com>
2023-10-22 12:04:13 +0800

    Slightly increase yMaxRotSpeed

--- src/main/java/com/EEEAB/EEEABMobs/sever/entity/ai/goal/GuardianShootLaserGoal.java
+++ src/main/java/com/EEEAB/EEEABMobs/sever/entity/ai/goal/GuardianShootLaserGoal.java
@@ -35,7 +35,7 @@ public class GuardianShootLaserGoal extends AnimationCommonGoal<EntityNamelessGu
                 this.entity.getLookControl().setLookAt(entityTarget, 90F, 90F);
             }
         } else if (tick >= 22 && tick < 100) {
-            float yMaxRotSpeed = 1 + Mth.clamp(1.8F - Math.abs(this.entity.targetDistance) * 0.1F, 0F, 1.8F);
+            float yMaxRotSpeed = 1 + Mth.clamp(2F - Math.abs(this.entity.targetDistance) * 0.1F, 0F, 2F);
             float xMaxRotAngle = 90F;
             if (entityTarget != null) {
                 this.entity.getLookControl().setLookAt(entityTarget.getX(), entityTarget.getY() + entityTarget.getBbHeight() / 2, entityTarget.getZ(), yMaxRotSpeed, xMaxRotAngle);
